fc00e22b74b11bccb3620c36efc65e53959e2590
Not Tofu Food (Aiden Thakurdial) <53190357+NotTofuFood@users.noreply.github.com>
2021-06-19 17:37:37 -0500

    Fixed Circle Generation + Radius Fixes

--- src/client/Canvas.java
+++ src/client/Canvas.java
@@ -174,21 +174,6 @@ public class Canvas extends JFrame implements  MouseMotionListener, ActionListen
     	
     	repaint();
     }
-    
-    public void drawErase(int x, int y, int length){
-    	System.out.println("got erase from server");
-    	this.oldX = this.x;
-    	this.oldY = this.y;
-    	this.oldCol = this.col;
-    	this.oldCheckErase = this.checkErase;
-    	this.revertOld = true;
-
-    	this.x = x;
-    	this.y = y;
-    	this.checkErase = true;
-
-    	repaint();
-    }
 
     public static void main (String args[])
     {
