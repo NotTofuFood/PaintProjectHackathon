3fe9f789dfb9c2d2d45c1c175115d0d92043e04f
Julian <jul204040204625@gmail.com>
2021-06-19 21:25:28 -0400

    fix black eraser

--- src/client/Painter.java
+++ src/client/Painter.java
@@ -58,6 +58,7 @@ public class Painter extends JPanel implements  MouseMotionListener, ActionListe
         }
         else {
             col =Color.BLACK;
+            checkErase = false;
         }
     }
 
